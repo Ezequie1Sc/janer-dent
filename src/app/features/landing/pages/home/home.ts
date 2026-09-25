e19e998e4751f8444eda516afd1d4d31ad9f200f
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 00:22:01 -0600

    feat: connect landing page components

--- src/app/features/landing/pages/home/home.ts
+++ src/app/features/landing/pages/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero';
+import { Services } from '../../components/services/services';
+import { About } from '../../components/about/about';
+import { Benefits } from '../../components/benefits/benefits';
+import { Gallery } from '../../components/gallery/gallery';
+import { Location } from '../../components/location/location';
+import { Contact } from '../../components/contact/contact';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
-  imports: [],
   selector: 'app-home',
-  styleUrl: './home.scss',
+  imports: [
+    Navbar,
+    Hero,
+    Services,
+    About,
+    Benefits,
+    Gallery,
+    Location,
+    Contact,
+    Footer,
+  ],
   templateUrl: './home.html',
+  styleUrl: './home.scss',
 })
 export class Home {}
