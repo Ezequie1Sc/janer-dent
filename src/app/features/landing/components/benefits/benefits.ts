f4c332ed59a96ae489c80a21a10b5601085b058a
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 00:04:41 -0600

    chore: create landing page structure

---
+++ src/app/features/landing/components/benefits/benefits.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-benefits',
+  styleUrl: './benefits.scss',
+  templateUrl: './benefits.html',
+})
+export class Benefits {}
---
+++ src/app/features/landing/components/location/location.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-location',
+  styleUrl: './location.scss',
+  templateUrl: './location.html',
+})
+export class Location {}
