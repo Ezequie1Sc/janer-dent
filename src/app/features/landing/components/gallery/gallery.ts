803bf40c6b608a336f6c63f5a490f8087279f32a
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 19:07:45 -0600

    feat: add gallery section logic

--- src/app/features/landing/components/gallery/gallery.ts
+++ src/app/features/landing/components/gallery/gallery.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  imports: [],
   selector: 'app-gallery',
-  styleUrl: './gallery.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './gallery.html',
+  styleUrl: './gallery.scss',
 })
-export class Gallery {}
+export class Gallery {
+
+
+  images = [
+
+    {
+      title:'Restauraciones dentales',
+      image:'https://images.unsplash.com/photo-1606811841689-23dfddce3e95?auto=format&fit=crop&w=900&q=80'
+    },
+
+    {
+      title:'Blanqueamiento dental',
+      image:'https://images.unsplash.com/photo-1609840114035-3c981b782dfe?auto=format&fit=crop&w=900&q=80'
+    },
+
+    {
+      title:'Tratamientos estéticos',
+      image:'https://images.unsplash.com/photo-1588776814546-daab30f3109a?auto=format&fit=crop&w=900&q=80'
+    },
+
+    {
+      title:'Cuidado profesional',
+      image:'https://images.unsplash.com/photo-1551601651-2a8555f1a136?auto=format&fit=crop&w=900&q=80'
+    }
+
+  ];
+
+
+}
