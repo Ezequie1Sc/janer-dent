311d964c265a76a26c243820056819452ff9dcee
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 18:34:27 -0600

    feat: add hero section logic

--- src/app/features/landing/components/hero/hero.ts
+++ src/app/features/landing/components/hero/hero.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-hero',
-  styleUrl: './hero.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './hero.html',
+  styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+
+
+  scrollY = signal(0);
+
+
+  @HostListener('window:scroll')
+  onScroll(){
+
+    this.scrollY.set(window.scrollY);
+
+  }
+
+
+  get imageTransform(){
+
+    return `scale(${1 + this.scrollY() * 0.00015}) 
+            translateY(${this.scrollY() * 0.18}px)`;
+
+  }
+
+
+  get textTransform(){
+
+    return `translateY(-${this.scrollY() * 0.15}px)`;
+
+  }
+
+
+  get textOpacity(){
+
+    const opacity = 1 - this.scrollY() / 600;
+
+    return opacity < 0 ? 0 : opacity;
+
+  }
+
+
+}
