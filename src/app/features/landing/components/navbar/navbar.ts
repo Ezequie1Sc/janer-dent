01a5928b14fa3b3032622b4917b1560cc5defa50
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 18:34:55 -0600

    feat: add navbar interactions

--- src/app/features/landing/components/navbar/navbar.ts
+++ src/app/features/landing/components/navbar/navbar.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [],
   selector: 'app-navbar',
-  styleUrl: './navbar.scss',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+
+  menuOpen = signal(false);
+  scrolled = signal(false);
+
+  navItems = [
+    { name: 'Inicio', link: '#inicio' },
+    { name: 'Servicios', link: '#servicios' },
+    { name: 'Nosotros', link: '#nosotros' },
+    { name: 'Galería', link: '#galeria' },
+    { name: 'Ubicación', link: '#ubicacion' },
+    { name: 'Contacto', link: '#contacto' }
+  ];
+
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled.set(window.scrollY > 40);
+  }
+
+
+  toggleMenu() {
+    this.menuOpen.update(value => !value);
+  }
+
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+}
