3a7448c281779dd0cec99625966f4174155d6a1d
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 18:33:58 -0600

    feat: add about section logic

--- src/app/features/landing/components/about/about.ts
+++ src/app/features/landing/components/about/about.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-about',
-  styleUrl: './about.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './about.html',
+  styleUrl: './about.scss',
 })
-export class About {}
+export class About implements AfterViewInit, OnDestroy {
+
+  @ViewChild('aboutSection')
+  section!: ElementRef<HTMLElement>;
+
+  private observer?: IntersectionObserver;
+
+  indicators = [
+    {
+      title: 'Atención personalizada',
+      text: 'Cada paciente recibe un tratamiento adaptado a sus necesidades.'
+    },
+    {
+      title: 'Trato humano',
+      text: 'Creamos una experiencia cómoda y de confianza.'
+    },
+    {
+      title: 'Resultados con confianza',
+      text: 'Utilizamos técnicas profesionales para cuidar tu sonrisa.'
+    }
+  ];
+
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          this.section.nativeElement.classList.add('visible');
+
+          // Solo necesitamos animarla una vez.
+          this.observer?.unobserve(this.section.nativeElement);
+        }
+      },
+      {
+        threshold: 0.2
+      }
+    );
+
+    this.observer.observe(this.section.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
+}
