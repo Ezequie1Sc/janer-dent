e18c419a69594e7b7e23f1b9409a0a485e9ed139
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 19:07:46 -0600

    feat: add location section logic

--- src/app/features/landing/components/location/location.ts
+++ src/app/features/landing/components/location/location.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  imports: [],
   selector: 'app-location',
-  styleUrl: './location.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './location.html',
+  styleUrl: './location.scss',
 })
-export class Location {}
+export class Location {
+
+
+  schedules = [
+
+    {
+      day:'Lunes a Viernes',
+      hours:[
+        '8:30 a.m. - 1:00 p.m.',
+        '5:00 p.m. - 9:00 p.m.'
+      ]
+    },
+
+    {
+      day:'Sábado',
+      hours:[
+        '9:00 a.m. - 1:00 p.m.',
+        '5:00 p.m. - 9:00 p.m.'
+      ]
+    }
+
+  ];
+
+
+
+}
