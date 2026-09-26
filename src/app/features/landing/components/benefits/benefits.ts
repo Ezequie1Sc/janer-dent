c21a5c1d67cfa5658211febdcc2e5343e9ce5820
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 19:07:41 -0600

    feat: add benefits section logic

--- src/app/features/landing/components/benefits/benefits.ts
+++ src/app/features/landing/components/benefits/benefits.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-benefits',
-  styleUrl: './benefits.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './benefits.html',
+  styleUrl: './benefits.scss',
 })
-export class Benefits {}
+export class Benefits {
+
+  benefits = [
+    {
+      number: '01',
+      title: 'Atención personalizada',
+      description:
+        'Cada tratamiento es diseñado pensando en las necesidades de cada paciente.'
+    },
+    {
+      number: '02',
+      title: 'Tecnología avanzada',
+      description:
+        'Utilizamos herramientas y técnicas actuales para ofrecer mejores resultados.'
+    },
+    {
+      number: '03',
+      title: 'Experiencia profesional',
+      description:
+        'Conocimiento y preparación para brindar tratamientos seguros y de calidad.'
+    },
+    {
+      number: '04',
+      title: 'Trato humano',
+      description:
+        'Creamos un ambiente cómodo basado en confianza, respeto y cercanía.'
+    },
+    {
+      number: '05',
+      title: 'Calidad',
+      description:
+        'Cuidamos cada detalle para ofrecer una experiencia odontológica superior.'
+    },
+    {
+      number: '06',
+      title: 'Seguridad',
+      description:
+        'Procesos profesionales enfocados en el bienestar de cada paciente.'
+    }
+  ];
+
+}
