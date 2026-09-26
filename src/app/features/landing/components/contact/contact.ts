2897328d3c40c17bd0fb050c88ed91fcabcf1efc
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-09-25 19:07:42 -0600

    feat: add contact section logic

--- src/app/features/landing/components/contact/contact.ts
+++ src/app/features/landing/components/contact/contact.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  imports: [],
   selector: 'app-contact',
-  styleUrl: './contact.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './contact.html',
+  styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+
+
+  whatsapp = '529961046096';
+
+
+}
